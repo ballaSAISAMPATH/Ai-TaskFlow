392bd148b336a79ca56d7e46fd88a65924959721
praveen <praveengamini009@gmail.com>
2025-07-27 02:07:35 +0530

    post issue is resolved

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ const taskRoutes = require('./routes/taskRoutes');
 const authRouter = require('./routes/auth-routes');
 
 const app = express();
+const PORT=process.env.PORT
 
 app.use(cors({
   origin: process.env.CLIENT_ORIGIN, 
@@ -26,11 +27,10 @@ mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log('MongoDB connected');
-    const PORT = process.env.PORT || 5000;
-    app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`);
-    });
   })
   .catch((err) => {
     console.error(err);
   });
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
