51a1ee8f573d4bfd977c72fc657e92c33e8fb82d
praveen <praveengamini009@gmail.com>
2025-08-15 04:22:02 +0530

    unecessary margins and result check in register component checked

--- front-end/src/pages/auth/Login.jsx
+++ front-end/src/pages/auth/Login.jsx
@@ -21,14 +21,17 @@ const handleLogin = async (e) => {
 
   try {
     const result = await dispatch(login({ email, password }));
-    if(result.payload.success)
+    if(result)
     {
-      toast.success(result.payload.message);
+      if(result.payload.success)
+      {
+        toast.success(result.payload.message);
+      }
+      else{
+        toast.error(result.payload.message)
+      }
+
     }
-    else{
-      toast.error(result.payload.message)
-    }
-    console.log("User data:", result);
   } catch (err) {
     toast.error(err?.message || "Login failed");
     console.error("Login error:", err);
@@ -38,7 +41,7 @@ const handleLogin = async (e) => {
 };
 
   return (
-    <div className="w-full max-w-md mx-auto mb-[20rem]">
+    <div className="w-full max-w-md mx-auto ">
       <div className="text-center mb-8">
         <h2 className="text-3xl font-bold text-gray-900 mb-2">Welcome Back</h2>
         <p className="text-gray-600">
--- front-end/src/pages/auth/Register.jsx
+++ front-end/src/pages/auth/Register.jsx
@@ -82,7 +82,7 @@ const Register = () => {
   }
 
   return (
-    <div className="w-full max-w-md mx-auto mt-[-7rem]">
+    <div className="w-full max-w-md mx-auto">
       <div className="text-center ">
         <h2 className="text-3xl font-bold text-gray-900 mb-2">Create Account</h2>
         <p className="text-gray-600">
