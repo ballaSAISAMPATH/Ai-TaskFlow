c6f4aec52d3bb5dcce66777ec268fd11aab58c88
ballaSAISAMPATH <saisampath453@gmail.com>
2025-08-15 23:02:08 +0530

    default user role

--- server/models/User.js
+++ server/models/User.js
@@ -36,6 +36,11 @@ const userSchema = new mongoose.Schema({
     type: String,
     enum: ['email', 'google'],
     default: 'email'
+  },
+  role: {
+    type: String,
+    enum: ['user', 'admin'],
+    default: 'user'
   }
 }, {
   timestamps: true
