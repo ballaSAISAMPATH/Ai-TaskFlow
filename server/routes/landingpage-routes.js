d40f3b9d2847ffecb2cdfb061c687b6af52c4d0e
ballaSAISAMPATH <saisampath453@gmail.com>
2025-08-17 19:01:15 +0530

    removed unecessary routes

--- server/routes/landingpage-routes.js
+++ server/routes/landingpage-routes.js
@@ -1,10 +1,8 @@
 const express = require('express');
-const { getLandingStats, getUserStats } = require('../controllers/statsController');
+const { getLandingStats } = require('../controllers/statsController');
 
 const router = express.Router();
 
 router.get('/landing', getLandingStats);
-
-router.get('/user', getUserStats);
 
 module.exports = router;
