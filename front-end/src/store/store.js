4509d771d42e48b65536137af101b500af289663
praveen <praveengamini009@gmail.com>
2025-08-17 19:15:45 +0530

    redux store set up done

--- front-end/src/store/store.js
+++ front-end/src/store/store.js
@@ -3,7 +3,9 @@ import authReducer from './auth/index.js';
 import taskReducer from './task/index.js';
 import feedbackReducer from './feedback/index.js';
 import adminReducer from './admin/index.js'
+import statsReducer from './landing-page/index.js'; 
 const rootReducer = combineReducers({
+  stats: statsReducer,
   auth: authReducer,
   task:taskReducer,
   feedback: feedbackReducer,
