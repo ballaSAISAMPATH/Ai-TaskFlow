7e6481ddf63ffcfd2f5b2910e80179900712c13d
ballaSAISAMPATH <saisampath453@gmail.com>
2025-08-21 03:14:51 +0530

    model modified to refer feedback

--- server/models/tetstimonials.js
+++ server/models/tetstimonials.js
@@ -17,11 +17,18 @@ const testimonialSchema = new mongoose.Schema({
     max: 5,
     default: 5
   },
+  feedbackId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Feedback',
+    default: null
+  },
   createdAt: {
     type: Date,
     default: Date.now
   }
 });
 
+testimonialSchema.index({ feedbackId: 1 }, { unique: true, sparse: true });
+
 const Testimonial = mongoose.model("Testimonial", testimonialSchema);
-module.exports.default = Testimonial;
+module.exports = Testimonial;
