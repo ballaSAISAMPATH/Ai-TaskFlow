d13f120761b451ef38071fd2e54600d1e5131f58
praveen <praveengamini009@gmail.com>
2025-08-21 07:08:44 +0530

    removed debugging

--- server/controllers/testimonials-controller.js
+++ server/controllers/testimonials-controller.js
@@ -3,7 +3,6 @@ const Feedback = require("../models/feedback"); // Add this import
 
 // Get all testimonials (public route)
 const getAllTestimonials = async (req, res) => {
-  console.log(req.body);
   
   try {
     const testimonials = await Testimonial.find()
