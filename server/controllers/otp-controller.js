728fb5bd3c953738e55bf05cc2e28bb1274ee5ee
ballaSAISAMPATH <saisampath453@gmail.com>
2025-08-25 17:23:51 +0530

    changed to transporter for createTransport

--- server/controllers/otp-controller.js
+++ server/controllers/otp-controller.js
@@ -2,15 +2,14 @@ const nodemailer = require('nodemailer');
 const crypto = require('crypto');
 const User = require('../models/User')
 const createTransporter = () => {
-  return nodemailer.createTransporter({
-    service: process.env.EMAIL_SERVICE || 'gmail', 
+  return nodemailer.createTransport({
+    service: process.env.EMAIL_SERVICE || "gmail",
     auth: {
       user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS, 
+      pass: process.env.EMAIL_PASS,
     },
   });
 };
-
 const generateOTP = () => {
   return crypto.randomInt(100000, 999999).toString();
 };
