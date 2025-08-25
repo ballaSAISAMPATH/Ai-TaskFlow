2abe46b1a32599ca6e2b0ea30ea0307e8165eac5
ballaSAISAMPATH <saisampath453@gmail.com>
2025-08-25 05:55:54 +0530

    Forgot Password page created

---
+++ front-end/src/pages/auth/ForgotPassword.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import SendOtp from '@/components/Auth/ForgotPassword/SendOtp'
+import VerifyOtp from '@/components/Auth/ForgotPassword/VerifyOtp'
+const ForgotPassword = () => {
+  return (
+    <div>
+      This is forgot password page
+    </div>
+  )
+}
+
+export default ForgotPassword
