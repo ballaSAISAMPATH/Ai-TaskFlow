8a92f758b97ab3372df71261755cbda865d842ff
praveen <praveengamini009@gmail.com>
2025-09-08 03:56:03 +0530

    horizontal spacing adjusted

--- front-end/src/utilities/hooks/GoalMap/usePathGeneration.js
+++ front-end/src/utilities/hooks/GoalMap/usePathGeneration.js
@@ -25,7 +25,7 @@ export const usePathGeneration = (levelCount) => {
       
       for (let row = 0; row < rows && currentLevel < levelCount; row++) {
         const levelsInThisRow = Math.min(optimalLevelsPerRow, levelCount - currentLevel)
-        const horizontalSpacing = usableWidth / (levelsInThisRow + 1)
+        const horizontalSpacing = usableWidth / (levelsInThisRow+1)
         const y = mapHeight - margin - (row * verticalSpacing)
         
         for (let col = 0; col < levelsInThisRow && currentLevel < levelCount; col++) {
