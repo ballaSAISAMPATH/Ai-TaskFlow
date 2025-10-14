988d845f54ad57cda4f116e4928b042e97aff3fa
ballaSAISAMPATH <saisampath453@gmail.com>
2025-10-14 17:54:48 +0530

    existing roadmaps page and roadmaps display updated

--- front-end/src/pages/UserPages/RoadMap/Roadmap.jsx
+++ front-end/src/pages/UserPages/RoadMap/Roadmap.jsx
@@ -12,7 +12,7 @@ export default function RoadMap() {
   })
   return (
     <div className='relative'>
-        <div className='flex gap-4 p-5 fixed m-0  top-18 bg-white z-999 w-full justify-center sm:justify-start'>
+        <div className='flex gap-4 p-5 fixed m-0  top-15 bg-white z-999 w-full justify-center sm:justify-start'>
 
         <Link className={`${location.pathname=="/user/road-map"?`bg-gray-300   text-black`:`bg-green-500 text-white hover:bg-green-700`} text-[13px] p-2  hover:shadow-xl hover:-translate-y-1 transition-all rounded-lg shadow-lg/50 shadow-black`} to="/user/road-map">existing roadmaps</Link>
         <Link className={`${location.pathname=="/user/road-map/create-roadmap"?`bg-gray-300 text-black`:`bg-green-500 text-white hover:bg-green-700`} text-[13px] p-2  hover:shadow-xl hover:-translate-y-1 transition-all rounded-lg shadow-lg/50 shadow-black`} to="/user/road-map/create-roadmap">Create new roadmap</Link>
